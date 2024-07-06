df93f05c6df34dc61edd52378c9efeb676d6f99e
frylo0 <frylo.d.ts@gmail.com>
2024-07-06 13:24:29 +0300

    2B-2: Createt Button component inside storybook

--- .storybook/preview.css.ts
+++ .storybook/preview.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css';
 import { fonts } from '../src/styles/bundle.css';
 
 const storybookDecorator = style({
-	fontFamily: fonts.gothamPro,
+	fontFamily: fonts.agrandirNormal,
 });
 
 export const s = {
--- src/styles/bundle.css.ts
+++ src/styles/bundle.css.ts
@@ -1,2 +1,3 @@
 export { media } from './vars/media.css';
 export { fonts } from './vars/fonts.css';
+export { colors } from './vars/color.css';
